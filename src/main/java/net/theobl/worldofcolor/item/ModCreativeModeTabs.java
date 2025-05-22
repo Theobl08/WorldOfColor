13c49d1b47732b734c058bd1a2b9202fa6020128
Theobl08 <theobl08@gmail.com>
2025-05-22 17:22:48 +0200

    Reorder items in creative tab

--- src/main/java/net/theobl/worldofcolor/item/ModCreativeModeTabs.java
+++ src/main/java/net/theobl/worldofcolor/item/ModCreativeModeTabs.java
@@ -5,10 +5,12 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.*;
 import net.minecraft.world.level.block.Block;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import net.theobl.worldofcolor.WorldOfColor;
 import net.theobl.worldofcolor.block.ModBlocks;
+import net.theobl.worldofcolor.util.ModUtil;
 
 public class ModCreativeModeTabs {
     // Create a Deferred Register to hold CreativeModeTabs which will all be registered under the "worldofcolor" namespace
@@ -20,6 +22,39 @@ public class ModCreativeModeTabs {
                     .withTabsBefore(CreativeModeTabs.COMBAT)
                     .icon(() -> ModBlocks.QUILTED_CONCRETES.getFirst().get().asItem().getDefaultInstance())
                     .displayItems((parameters, output) -> {
+                        for (DyeColor color : ModUtil.COLORS) {
+                            int index = ModUtil.COLORS.indexOf(color);
+                            output.accept(ModBlocks.COLORED_LEAVES.get(index));
+                            output.accept(ModBlocks.COLORED_LOGS.get(index));
+                            output.accept(ModBlocks.COLORED_WOODS.get(index));
+                            output.accept(ModBlocks.COLORED_STRIPPED_LOGS.get(index));
+                            output.accept(ModBlocks.COLORED_STRIPPED_WOODS.get(index));
+                            output.accept(ModBlocks.COLORED_PLANKS.get(index));
+                            output.accept(ModBlocks.COLORED_STAIRS.get(index));
+                            output.accept(ModBlocks.COLORED_SLABS.get(index));
+                            output.accept(ModBlocks.COLORED_FENCES.get(index));
+                            output.accept(ModBlocks.COLORED_FENCE_GATES.get(index));
+                            output.accept(ModBlocks.COLORED_DOORS.get(index));
+                            output.accept(ModBlocks.COLORED_TRAPDOORS.get(index));
+                            output.accept(ModBlocks.COLORED_PRESSURE_PLATES.get(index));
+                            output.accept(ModBlocks.COLORED_BUTTONS.get(index));
+                            output.accept(ModBlocks.COLORED_SIGNS.get(index));
+                            output.accept(ModBlocks.COLORED_HANGING_SIGNS.get(index));
+                            output.accept(ModItems.COLORED_BOATS.get(index));
+                            output.accept(ModItems.COLORED_CHEST_BOATS.get(index));
+                        }
+                        for (DyeColor color : ModUtil.COLORS) {
+                            int index = ModUtil.COLORS.indexOf(color);
+                            output.accept(ModBlocks.COLORED_COPPER_BLOCKS.get(index));
+                            output.accept(ModBlocks.COLORED_CHISELED_COPPER.get(index));
+                            output.accept(ModBlocks.COLORED_COPPER_GRATES.get(index));
+                            output.accept(ModBlocks.COLORED_CUT_COPPER.get(index));
+                            output.accept(ModBlocks.COLORED_CUT_COPPER_STAIRS.get(index));
+                            output.accept(ModBlocks.COLORED_CUT_COPPER_SLABS.get(index));
+                            output.accept(ModBlocks.COLORED_COPPER_DOORS.get(index));
+                            output.accept(ModBlocks.COLORED_COPPER_TRAPDOORS.get(index));
+                            output.accept(ModBlocks.COLORED_COPPER_BULBS.get(index));
+                        }
                         for (DeferredHolder<Block, ? extends Block> block : ModBlocks.BLOCKS.getEntries()) {
                             output.accept(block.get());
                         }
