1e7d3e5562ccff69373b1650a097f431f2e3904f
Theobl08 <theobl08@gmail.com>
2025-09-06 18:37:24 +0200

    Update DataGenerators.java

--- src/main/java/net/theobl/worldofcolor/datagen/DataGenerators.java
+++ src/main/java/net/theobl/worldofcolor/datagen/DataGenerators.java
@@ -1,39 +1,28 @@
 package net.theobl.worldofcolor.datagen;
 
-import net.minecraft.core.HolderLookup;
-import net.minecraft.data.DataGenerator;
-import net.minecraft.data.PackOutput;
 import net.minecraft.data.loot.LootTableProvider;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.neoforge.common.data.BlockTagsProvider;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 import net.theobl.worldofcolor.WorldOfColor;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
 
 @EventBusSubscriber(modid = WorldOfColor.MODID, bus = EventBusSubscriber.Bus.MOD)
 public class DataGenerators {
     @SubscribeEvent
     public static void gatherData(GatherDataEvent.Client event) {
-        DataGenerator generator = event.getGenerator();
-        PackOutput packOutput = generator.getPackOutput();
-        CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
-
-        generator.addProvider(true, new ModRecipeProvider.Runner(packOutput, lookupProvider));
-        BlockTagsProvider blockTagsProvider = new ModBlockTagsProvider(packOutput, lookupProvider);
-        generator.addProvider(true, blockTagsProvider);
-        generator.addProvider(true, new ModItemTagsProvider(packOutput, lookupProvider, blockTagsProvider.contentsGetter()));
-        generator.addProvider(true, new ModEntityTypeTagsProvider(packOutput, lookupProvider));
-        generator.addProvider(true, new LootTableProvider(packOutput, Collections.emptySet(),
+        event.createProvider(ModRecipeProvider.Runner::new);
+        event.createBlockAndItemTags(ModBlockTagsProvider::new, ModItemTagsProvider::new);
+        event.createProvider(ModEntityTypeTagsProvider::new);
+        event.createProvider((output, lookupProvider) -> new LootTableProvider(output, Collections.emptySet(),
                 List.of(new LootTableProvider.SubProviderEntry(ModBlockLootTableProvider::new, LootContextParamSets.BLOCK)), lookupProvider));
-        generator.addProvider(true, new ModDataMapProvider(packOutput, lookupProvider));
-        generator.addProvider(true, new ModDatapackBuiltInEntriesProvider(packOutput, lookupProvider));
+        event.createProvider(ModDataMapProvider::new);
+        event.createProvider(ModDatapackBuiltInEntriesProvider::new);
 
-        generator.addProvider(true, new ModModelProvider(packOutput));
-        generator.addProvider(true, new ModLanguageProvider(packOutput));
+        event.createProvider(ModModelProvider::new);
+        event.createProvider(ModLanguageProvider::new);
     }
 }
