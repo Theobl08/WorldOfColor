0d6486dd5c3c177cea0c5ddb01b13535b0322962
Theobl08 <98655331+Theobl08@users.noreply.github.com>
2025-11-06 19:21:50 +0100

    Simplify ModLanguageProvider

--- src/main/java/net/theobl/worldofcolor/datagen/ModLanguageProvider.java
+++ src/main/java/net/theobl/worldofcolor/datagen/ModLanguageProvider.java
@@ -4,16 +4,17 @@ import net.minecraft.data.PackOutput;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.block.*;
+import net.neoforged.neoforge.common.Tags;
 import net.neoforged.neoforge.common.data.LanguageProvider;
-import net.neoforged.neoforge.registries.DeferredHolder;
 import net.theobl.worldofcolor.WorldOfColor;
 import net.theobl.worldofcolor.block.ModBlocks;
 import net.theobl.worldofcolor.entity.ModEntityType;
 import net.theobl.worldofcolor.item.ModItems;
 import net.theobl.worldofcolor.tags.ModTags;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.function.Supplier;
 
 public class ModLanguageProvider extends LanguageProvider {
     public ModLanguageProvider(PackOutput output) {
@@ -23,26 +24,18 @@ public class ModLanguageProvider extends LanguageProvider {
     @Override
     protected void addTranslations() {
         add("itemGroup.worldofcolor", "World of Color");
-        for (DeferredHolder<Block, ? extends Block> block : ModBlocks.BLOCKS.getEntries()) {
-            if(block.get() instanceof WallSignBlock || block.get() instanceof WallHangingSignBlock)
-                continue;
-            add(block.get(), capitalizeString(filterBlockLang(block.get())));
-        }
+        ModBlocks.BLOCKS.getEntries().forEach(this::lang);
 
-        ModItems.COLORED_BOATS.forEach(item -> add(item.asItem(), capitalizeString(filterItemLang(item))));
-        ModItems.COLORED_CHEST_BOATS.forEach(item -> add(item.asItem(), filterChestBoatLang(item)));
-        ModItems.COLORED_ITEM_FRAMES.forEach(item -> add(item.asItem(), capitalizeString(filterItemLang(item))));
-        ModEntityType.COLORED_BOATS.forEach(boat -> add(boat.get(), capitalizeString(filterEntityTypeLang(boat.get()))));
-        ModEntityType.COLORED_CHEST_BOATS.forEach(boat -> add(boat.get(), filterChestBoatLang(boat.get())));
-        ModEntityType.COLORED_ITEM_FRAMES.forEach(type -> add(type.get(), capitalizeString(filterEntityTypeLang(type.get()))));
+        ModItems.COLORED_BOATS.forEach(this::lang);
+        ModItems.COLORED_CHEST_BOATS.forEach(this::lang);
+        ModItems.COLORED_ITEM_FRAMES.forEach(this::lang);
+        ModEntityType.COLORED_BOATS.forEach(this::lang);
+        ModEntityType.COLORED_CHEST_BOATS.forEach(this::lang);
+        ModEntityType.COLORED_ITEM_FRAMES.forEach(this::lang);
 
-        for (TagKey<Block> tag : ModTags.Blocks.COLORED_LOGS) {
-            add(tag, capitalizeString(filterTagLang(tag)));
-        }
+        ModTags.Blocks.COLORED_LOGS.forEach(this::lang);
 
-        for (TagKey<Item> tag : ModTags.Items.COLORED_LOGS) {
-            add(tag, capitalizeString(filterTagLang(tag)));
-        }
+        ModTags.Items.COLORED_LOGS.forEach(this::lang);
 
 //        for (Field field : BlockTags.class.getDeclaredFields()) {
 //            TagKey<Block> tag = null;
@@ -51,7 +44,7 @@ public class ModLanguageProvider extends LanguageProvider {
 //            } catch (IllegalAccessException e) {
 //                throw new IllegalStateException(BlockTags.class.getName() + " is missing tag name: " + field.getName());
 //            }
-//            add(tag, capitalizeString(filterTagLang(tag)));
+//            lang(tag);
 //        }
     }
 
@@ -69,38 +62,29 @@ public class ModLanguageProvider extends LanguageProvider {
         return String.valueOf(chars);
     }
 
-    private static @NotNull String filterBlockLang(@NotNull Block key) {
-        return key.getDescriptionId().replace("block.worldofcolor.","").replace("_"," ");
-    }
+    private <T> void lang(T key) {
+        String descriptionId = switch (key) {
+            case Block block -> block.getDescriptionId();
+            case Item item -> item.getDescriptionId();
+            case EntityType<?> entityType -> entityType.getDescriptionId();
+            case TagKey<?> tagKey -> Tags.getTagTranslationKey(tagKey);
+            case null, default -> "";
+        };
 
-    private static @NotNull String filterTagLang(@NotNull TagKey<?> key) {
-        return key.location().getPath().replace("_"," ");
-    }
-
-    private static @NotNull String filterItemLang(ItemLike key){
-        return key.asItem().getDescriptionId().replace("item.worldofcolor.","").replace("_"," ")
-                .replace("block.worldofcolor.","");
-    }
-
-    private static String filterChestBoatLang(ItemLike key){
-        String type = key.asItem().getDescriptionId()
+        String value = capitalizeString(descriptionId.replace("block.worldofcolor.","")
                 .replace("item.worldofcolor.","")
-                .replace("chest_boat","")
-                .replace("_"," ");
-
-        return capitalizeString(type) + "Boat with Chest";
-    }
-
-    private static String filterChestBoatLang(EntityType<?> key){
-        String type = key.getDescriptionId()
                 .replace("entity.worldofcolor.","")
+                .replace("tag.", "")
                 .replace("chest_boat","")
-                .replace("_"," ");
+                .replace("_"," "));
 
-        return capitalizeString(type) + "Boat with Chest";
+        if(descriptionId.contains("chest_boat")) {
+            value += "Boat with Chest";
+        }
+        add(descriptionId, value);
     }
 
-    private static String filterEntityTypeLang(EntityType<?> key){
-        return key.getDescriptionId().replace("entity.worldofcolor.","").replace("_"," ");
+    private <T> void lang(Supplier<T> key) {
+        lang(key.get());
     }
 }
