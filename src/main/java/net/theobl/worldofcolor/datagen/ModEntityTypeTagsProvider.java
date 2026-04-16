aad6ab8017a67604908c9d5d02aeaf637b872744
Theobl08 <theobl08@gmail.com>
2026-04-16 13:35:45 +0200

    Add missing tag for colored item frame
    Also move colored boat from "#c:boats" to "#minecraft:boat" entity type tag

--- src/main/java/net/theobl/worldofcolor/datagen/ModEntityTypeTagsProvider.java
+++ src/main/java/net/theobl/worldofcolor/datagen/ModEntityTypeTagsProvider.java
@@ -3,6 +3,7 @@ package net.theobl.worldofcolor.datagen;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.EntityTypeTagsProvider;
+import net.minecraft.tags.EntityTypeTags;
 import net.neoforged.neoforge.common.Tags;
 import net.theobl.worldofcolor.WorldOfColor;
 import net.theobl.worldofcolor.entity.ModEntityType;
@@ -16,7 +17,8 @@ public class ModEntityTypeTagsProvider extends EntityTypeTagsProvider {
 
     @Override
     protected void addTags(HolderLookup.Provider provider) {
-        ModEntityType.COLORED_BOATS.values().forEach(boat -> tag(Tags.EntityTypes.BOATS).add(boat.get()));
+        ModEntityType.COLORED_BOATS.values().forEach(boat -> tag(EntityTypeTags.BOAT).add(boat.get()));
         ModEntityType.COLORED_CHEST_BOATS.values().forEach(boat -> tag(Tags.EntityTypes.BOATS).add(boat.get()));
+        ModEntityType.COLORED_ITEM_FRAMES.values().forEach(boat -> tag(Tags.EntityTypes.ITEM_FRAMES).add(boat.get()));
     }
 }
